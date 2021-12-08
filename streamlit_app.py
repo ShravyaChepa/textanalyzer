abbdd788d9eb795abe0b729c3c48c3238eac45e7
ShravyaChepa <48849876+ShravyaChepa@users.noreply.github.com>
2021-12-09 01:36:18 +0530

    remove padding

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,15 +90,6 @@ div.stButton > button:first-child:hover {
 }
 </style>""", unsafe_allow_html=True)
 
-padding = 0
-st.markdown(f""" <style>
-    .reportview-container .main .block-container{{
-        padding-top: {padding}rem;
-        padding-right: {padding}rem;
-        padding-left: {padding}rem;
-        padding-bottom: {padding}rem;
-    }} </style> """, unsafe_allow_html=True)
-
 
 #heading
 image = Image.open('note.png')
